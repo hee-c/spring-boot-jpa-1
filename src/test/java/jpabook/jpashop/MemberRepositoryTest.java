94fe7c462a1f2cb84dee7bd5d09b9483aded3134
hee-c <ways2kim@gmail.com>
2024-06-27 22:23:41 +0900

    실전 스프링 부트와 JPA 활용 1 / ~ 섹션 1

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        // given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.find(savedId);
+
+        System.out.println("왜 안나옴??");
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: " + (findMember == member));
+    }
+}
